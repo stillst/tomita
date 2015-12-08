8a128b9ac0485c50b4992a08231e7dfb89458b38
stas.a.gavrilov@gmail.com <stas.a.gavrilov@gmail.com>
2015-12-08 16:37:05 +0300

    work start

--- dev/main.cxx
+++ dev/main.cxx
@@ -1,7 +1,8 @@
 #encoding "utf-8"
 #include <date.cxx>
 
-FIO -> Word<h-reg1, gram='имя'>
-              Word<h-reg1, gram='отч'>
-              Word<h-reg1, gram='фам'>;
-S -> (FIO) Date;
+FIO -> Word<h-reg1, gram='имя'> interp (FIO.Name)
+       Word<h-reg1, gram='отч'> interp (FIO.Surname)
+       Word<h-reg1, gram='фам'> interp (FIO.Patronymic);
+
+S -> FIO (Date);
