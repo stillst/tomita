922dbd7142a4305c9d3695a673cdafa625a004a7
stas.a.gavrilov@gmail.com <stas.a.gavrilov@gmail.com>
2015-12-10 18:18:21 +0300

    add Place

--- dev/places.cxx
+++ dev/places.cxx
@@ -1,7 +1,8 @@
 #encoding "utf-8"
 PlaceWord -> Noun<kwtype="место">;
 PlaceWord_ -> Noun<kwtype="место_">;
+Country -> Noun<kwtype="страна">;
 
-Place -> PlaceWord<gnc-agr[1]> Word<gnc-agr[1], h-reg1> | Word<gnc-agr[1], h-reg1> PlaceWord_<gnc-agr[1]> ;
+Place -> PlaceWord<gnc-agr[1]> Word<gnc-agr[1], h-reg1> | Word<gnc-agr[1], h-reg1> PlaceWord_<gnc-agr[1]> | Country;
 
 Placeinfo -> Place interp (Place.Name);
