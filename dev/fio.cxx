d4616c71110948a64541e9cb529fae9507fa9858
stas.a.gavrilov@gmail.com <stas.a.gavrilov@gmail.com>
2015-12-10 12:47:39 +0300

    work with FIO

--- dev/fio.cxx
+++ dev/fio.cxx
@@ -1,7 +1,21 @@
 FIO -> Word<h-reg1, gram='имя'> interp (FIO.Name)
-       Word<h-reg1, gram='отч'> interp (FIO.Surname)
-       Word<h-reg1, gram='фам'> interp (FIO.Patronymic)|
+       Word<h-reg1, gram='отч'> interp (FIO.Patronymic)
+       Word<h-reg1, gram='фам'> interp (FIO.Surname)|
+
        Word<h-reg1, gram='имя'> interp (FIO.Name)
+       Word<h-reg1, gram='фам'> interp (FIO.Surname)|
 
+       Word<h-reg1, gram='фам'> interp (FIO.Surname)
+       Word<h-reg1, gram='имя'> interp (FIO.Name)|
 
+       Word<h-reg1, gram='фам'> interp (FIO.Surname)
+       AnyWord<wff=/[А-Яа-я]{1}./>  interp (FIO.Name)
+       AnyWord<wff=/[А-Яа-я]{1}./>  interp (FIO.Patronymic)|
+
+       Word<h-reg1, gram='фам'> interp (FIO.Surname)
+       AnyWord<wff=/[А-Яа-я]{1}./>  interp (FIO.Name)|
+
+       Word<h-reg1, gram='имя'> interp (FIO.Name)|
+
+       Word<h-reg1, gram='фам'> interp (FIO.Surname)
        ;
