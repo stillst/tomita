dab8e7701d87922b480b72631366eb2e4b26e71d
stas.a.gavrilov@gmail.com <stas.a.gavrilov@gmail.com>
2015-12-14 12:06:03 +0300

    work with Country

--- dev/geopolitic.cxx
+++ dev/geopolitic.cxx
@@ -1,3 +1,13 @@
 #encoding "utf-8"
 
+
+Polit -> Noun<kwtype="гео_полит"> | Noun<kwtype="интро_полит">;
+
 Country -> Noun<kwtype="страна"> interp (GeoPolitic.Name);
+Country -> Polit<gnc-agr[1]> Word<h-reg1, gnc-agr[1]> interp (GeoPolitic.Name);
+
+//Парламент Великобритании
+PolitStruct  -> Noun<kwtype="интро_полит"> Word<kwtype="страна"> ;
+//Австралийский парламент (не работает)
+PolitStruct  -> Word<kwtype="страна"> Noun<kwtype="интро_полит">;
+Country -> PolitStruct interp (GeoPolitic.Name);
